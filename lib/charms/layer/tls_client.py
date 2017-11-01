291c057a45882a33175ec799a0aed0199080fdc8
Mike Wilson <knobby@burntsheep.com>
2017-11-01 17:44:56 -0400

    Adding tls_client.py file for external function for clearing the flag on certificate write

---
+++ lib/charms/layer/tls_client.py
@@ -0,0 +1,25 @@
+# Copyright 2016-2017 Canonical Ltd.
+#
+# This file is part of the Snap layer for Juju.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charms.reactive import remove_state
+
+# Reset the certificate written flag so notification will work on the next write
+# cert_type must be 'server', 'client', or 'ca' to indicate type of certificate
+def reset_certificate_write_flag(cert_type):
+    if not cert_type == 'server' and not cert_type == 'client' and not cert_type == 'ca':
+        log('Unknown certificate type!')
+    else:
+        remove_state('tls_client.{0}.certificate_written'.format(cert_type))
